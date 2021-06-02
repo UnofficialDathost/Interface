9f4ed7e662bb38efbf209f90e2114e2b937f6640
Ward <wardpearce@protonmail.com>
2021-06-02 17:45:20 +1200

    Update disclaimer.vue

--- src/components/disclaimer.vue
+++ src/components/disclaimer.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <p style="margin-bottom: 5px;">Data on this website is sent via a proxy to dathost.net/api.</p>
-    <p style="margin-bottom: 5px;">This website has zero&nbsp;affiliation with dathost.net.</p><a href="https://github.com/UnofficialDathost/Interface" target="_blank">Proudly licensed under&nbsp;GNU Affero 3</a>
+    <p style="margin-bottom: 5px;">No sensitive data is stored outside the browser's local storage.</p>
+    <p style="margin-bottom: 5px;">This website has zero&nbsp;affiliation with dathost.net.</p>
+    <a href="https://github.com/UnofficialDathost/Interface" target="_blank">Proudly licensed under&nbsp;GNU Affero 3</a>
   </div>
 </template>
 
