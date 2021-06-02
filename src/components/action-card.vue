baf97135b57bc1401ed2a846cda5fe7ed857994b
Ward <wardpearce@protonmail.com>
2021-06-02 15:19:54 +1200

    added server ordering

--- src/components/action-card.vue
+++ src/components/action-card.vue
@@ -2,7 +2,15 @@
     <div class="card">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-8">
+                    <b-dropdown class="m-md-2">
+                      <template #button-content>
+                        Order by: <strong>{{ selectedOrder }}</strong>
+                      </template>
+                      <div v-for="(order, index) in orders" :key="index">
+                        <b-dropdown-item @click="changeOrder(order)" v-if="order !== selectedOrder">{{ order }}</b-dropdown-item>
+                      </div>
+                    </b-dropdown>
                     <div v-if="bulkActions" class="btn-group" role="group">
                         <button @click="toggleActions()" class="btn btn-secondary" style="margin-right: 20px;" type="button"><b-icon icon="collection"></b-icon>&nbsp;Stop managing</button>
                         <button class="btn btn-primary" @click="$emit('startServers')" type="button"><b-icon icon="play-circle"></b-icon>&nbsp;Start</button>
@@ -13,7 +21,7 @@
                     </div>
                     <button @click="toggleActions()" v-else class="btn btn-secondary" type="button"><b-icon icon="collection"></b-icon>&nbsp;Manage Servers</button>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <!-- Start: Search Input (responsive) -->
                     <div class="float-left float-md-right mt-5 mt-md-0 search-area">
                         <b-icon class="float-left search-icon" icon="search"></b-icon>
@@ -33,10 +41,26 @@ import Component from 'vue-class-component'
 @Component({ name: 'ActionCard' })
 export default class ActionCardComp extends Vue {
   bulkActions = false
+  selectedOrder = 'Priority'
+  orders: string[] = [
+    'Priority',
+    'Cost',
+    'Name',
+    'Slots'
+  ]
+
+  created (): void {
+    this.$emit(`orderBy${this.orders[0]}`)
+  }
 
   toggleActions (): void {
     this.bulkActions = !this.bulkActions
     this.$emit('mangingServers', this.bulkActions)
+  }
+
+  changeOrder (order: string): void {
+    this.selectedOrder = order
+    this.$emit(`orderBy${order}`)
   }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,10 @@
     @cloneServers="cloneServers"
     @deleteServers="deleteServers"
     @mangingServers="toggleMangement"
+    @orderByPriority="orderByPriority"
+    @orderByCost="orderByCost"
+    @orderByName="orderByName"
+    @orderBySlots="orderBySlots"
     v-model="serverSearch"/>
     <div v-if="serversLoading" class="d-flex justify-content-center mb-3">
       <b-spinner style="width: 6rem; height: 6rem; margin-top: 25px;" label="Loading..."></b-spinner>
@@ -44,6 +48,7 @@ export default class HomeView extends VueMixin {
   serversLoading = true
   beingManged = false
   serverInterval: ReturnType<typeof setInterval>
+  orderFunc?: CallableFunction
 
   async mounted (): Promise<void> {
     await this.getServers()
@@ -56,7 +61,8 @@ export default class HomeView extends VueMixin {
 
     if (this.serverSearch !== '') {
       this.servers = this.servers.filter((server) => {
-        return this.searchHelper(this.serverSearch, server.name) || this.searchHelper(this.serverSearch, server.location) || this.searchHelper(this.serverSearch, server.game) || this.searchHelper(this.serverSearch, server.id)
+        return this.searchHelper(this.serverSearch, server.name) || this.searchHelper(this.serverSearch, server.location
+        ) || this.searchHelper(this.serverSearch, server.game) || this.searchHelper(this.serverSearch, server.id)
       })
     } else {
       await this.getServers()
@@ -66,6 +72,60 @@ export default class HomeView extends VueMixin {
 
   searchHelper (search: string, toSearch: string): boolean {
     return toSearch.toLowerCase().search(search.toLowerCase()) !== -1
+  }
+
+  orderByPriority (): void {
+    this.orderFunc = this.orderByPriority
+
+    this.servers = this.servers.sort((a, b) => {
+      return a.manual_sort_order - b.manual_sort_order
+    })
+  }
+
+  orderByCost (): void {
+    this.orderFunc = this.orderByCost
+
+    this.servers = this.servers.sort((a, b) => {
+      return b.max_cost_per_month - a.max_cost_per_month
+    })
+  }
+
+  orderByName (): void {
+    this.orderFunc = this.orderByName
+
+    this.servers = this.servers.sort((a, b) => {
+      return a.name < b.name ? -1 : 1
+    })
+  }
+
+  orderBySlots (): void {
+    this.orderFunc = this.orderBySlots
+
+    this.servers = this.servers.sort((a, b) => {
+      let aSlots: number
+      let bSlots: number
+      if (a.game === 'csgo') {
+        aSlots = a.csgo_settings.slots
+      } else if (a.game === 'teamfortress2') {
+        aSlots = a.teamfortress2_settings.slots
+      } else if (a.game === 'teamspeak3') {
+        aSlots = a.teamspeak3_settings.slots
+      } else {
+        aSlots = a.valheim_settings.slots
+      }
+
+      if (b.game === 'csgo') {
+        bSlots = b.csgo_settings.slots
+      } else if (b.game === 'teamfortress2') {
+        bSlots = b.teamfortress2_settings.slots
+      } else if (b.game === 'teamspeak3') {
+        bSlots = b.teamspeak3_settings.slots
+      } else {
+        bSlots = b.valheim_settings.slots
+      }
+
+      return bSlots - aSlots
+    })
   }
 
   beforeRouteLeave (to: Route, from: Route, next: FunctionConstructor): void {
@@ -89,6 +149,12 @@ export default class HomeView extends VueMixin {
       servers.push(server[0])
     }
     this.servers = servers
+
+    // If order func defined, call after updating server list.
+    // Easy solution to keeping updated servers filtered correctly.
+    if (typeof this.orderFunc !== 'undefined') {
+      this.orderFunc()
+    }
 
     this.serversLoading = false
   }
