85c8ab9b3f0dace07b112b9197258ae0d57f91d4
Ward <wardpearce@protonmail.com>
2021-06-08 01:25:37 +1200

    minor improvements

--- src/components/server-card.vue
+++ src/components/server-card.vue
@@ -1,11 +1,11 @@
 <template>
-  <div v-if="!deleted" class="col-md-4">
+  <div v-if="!deleted && serverCreated" class="col-md-4">
     <div class="card game-card" style="cursor: pointer;height: 95%;" v-bind:class="{'game-selected': selected}">
         <div class="card-header" @click="$emit('serverClicked', serverData)">
             <div class="row">
-                <div class="col-md-6 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="game-icon" :src="require(`@/assets/img/games/${serverData.game}.svg`)"></div>
+                <div class="col-md-6 d-xl-flex justify-content-xl-start align-items-xl-center"><img class="game-icon" :src="require(`@/assets/img/games/${server.game}.svg`)"></div>
                 <div class="col-md-6 d-xl-flex justify-content-xl-end align-items-xl-center">
-                    <h6 class="game-name">&nbsp;{{ serverData.name.substring(0, 19) }}<template v-if="serverData.name.length > 19">...</template></h6>
+                    <h6 class="game-name">&nbsp;{{ server.name.substring(0, 19) }}<template v-if="server.name.length > 19">...</template></h6>
                 </div>
             </div>
         </div>
@@ -19,7 +19,7 @@
           <div @click.self="$emit('serverClicked', serverData)" class="card-body d-flex d-xl-flex flex-column justify-content-center justify-content-xl-center" style="padding-bottom: 0px;">
               <div class="row" @click="$emit('serverClicked', serverData)">
                   <div class="col-md-6 col-xl-4 d-xl-flex justify-content-xl-start">
-                      <p class="text-capitalize"><b-icon icon="compass"></b-icon>&nbsp;{{ serverData.location.replaceAll('_', ' ') }}&nbsp;</p>
+                      <p class="text-capitalize"><b-icon icon="compass"></b-icon>&nbsp;{{ server.location.replaceAll('_', ' ') }}&nbsp;</p>
                   </div>
                   <div class="col-md-6 col-xl-4 d-xl-flex justify-content-xl-center">
                       <p>
@@ -28,7 +28,7 @@
                       </p>
                   </div>
                   <div class="col-md-6 col-xl-4 d-xl-flex justify-content-xl-end">
-                      <p><b-icon icon="lightning"></b-icon>&nbsp;{{ serverData.month_credits.toFixed(2) }} / {{ serverData.max_cost_per_month.toFixed(2) }}</p>
+                      <p><b-icon icon="lightning"></b-icon>&nbsp;{{ server.month_credits.toFixed(2) }} / {{ server.max_cost_per_month.toFixed(2) }}</p>
                   </div>
               </div>
           </div>
@@ -37,21 +37,21 @@
                   <button v-if="serverStatus.restarting" class="btn btn-primary" type="button">
                     <b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner>&nbsp;Restarting
                   </button>
-                  <button v-else-if="serverStatus.startingUp || serverData.booting" class="btn btn-primary" type="button">
+                  <button v-else-if="serverStatus.startingUp" class="btn btn-primary" type="button">
                     <b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner>&nbsp;Starting
                   </button>
-                  <button v-else-if="serverData.on === false" @click="startServer()" class="btn btn-primary" type="button"><b-icon icon="play-circle"></b-icon>&nbsp;Start</button>
-                  <template v-else>
+                  <template v-else-if="server.on === true">
                     <button v-if="serverStatus.stopping" class="btn btn-secondary" type="button">
                       <b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner>&nbsp;Stopping
                     </button>
                     <button v-else @click="stopServer()" class="btn btn-secondary" type="button"><b-icon icon="stop-circle"></b-icon>&nbsp;Stop</button>
                   </template>
-                  <b-dropdown v-if="serverData.game === 'csgo' || serverData.game === 'teamfortress2'" text="Connect" variant="secondary">
-                    <b-dropdown-item :href="`steam://connect/${serverData.raw_ip}:${serverData.ports.game}${steamProtocolPass}`"><b-icon icon="arrow-up-right-square-fill"></b-icon> Connect</b-dropdown-item>
-                    <b-dropdown-item @click="copyToClipboard(`connect ${serverData.ip}:${serverData.ports.game}${password}`)"><b-icon icon="clipboard"></b-icon> Dathost IP</b-dropdown-item>
-                    <b-dropdown-item @click="copyToClipboard(`connect ${serverData.raw_ip}:${serverData.ports.game}${password}`)"><b-icon icon="clipboard"></b-icon> Raw IP</b-dropdown-item>
-                    <b-dropdown-item v-if="gotvEnabled" @click="copyToClipboard(`connect ${serverData.raw_ip}:${serverData.ports.gotv}`)"><b-icon icon="clipboard"></b-icon> GOTV IP</b-dropdown-item>
+                  <button v-else @click="startServer()" class="btn btn-primary" type="button"><b-icon icon="play-circle"></b-icon>&nbsp;Start</button>
+                  <b-dropdown v-if="server.game === 'csgo' || server.game === 'teamfortress2'" text="Connect" variant="secondary">
+                    <b-dropdown-item :href="`steam://connect/${server.raw_ip}:${server.ports.game}${steamProtocolPass}`"><b-icon icon="arrow-up-right-square-fill"></b-icon> Connect</b-dropdown-item>
+                    <b-dropdown-item @click="copyToClipboard(`connect ${server.ip}:${server.ports.game}${password}`)"><b-icon icon="clipboard"></b-icon> Dathost IP</b-dropdown-item>
+                    <b-dropdown-item @click="copyToClipboard(`connect ${server.raw_ip}:${server.ports.game}${password}`)"><b-icon icon="clipboard"></b-icon> Raw IP</b-dropdown-item>
+                    <b-dropdown-item v-if="gotvEnabled" @click="copyToClipboard(`connect ${server.raw_ip}:${server.ports.gotv}`)"><b-icon icon="clipboard"></b-icon> GOTV IP</b-dropdown-item>
                   </b-dropdown>
                   <b-dropdown text="More" variant="secondary">
                     <b-dropdown-item @click="cloneServer()"><b-icon icon="file-break"></b-icon> Clone</b-dropdown-item>
@@ -95,8 +95,11 @@ export default class ServerCardComp extends mixins(VueMixin, ServerMixin) {
   @Prop({ type: Boolean, default: false })
   selected: boolean
 
+  serverCreated = false
+
   mounted (): void {
     this.createServer(this.serverData)
+    this.serverCreated = true
   }
 }
 </script>
