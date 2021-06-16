239fb678b57e16a020c308d4ad173405b6268eb8
Ward <wardpearce@protonmail.com>
2021-06-16 17:43:17 +1200

    improve loading

--- src/components/server-files.vue
+++ src/components/server-files.vue
@@ -1,82 +1,80 @@
 <template>
   <div>
-    <div v-if="treeLoaded">
-      <div class="d-flex justify-content-between" style="margin-bottom:5px;">
-        <div class="search-area">
-            <b-icon class="float-left search-icon" icon="search"></b-icon>
-            <input @input="inputDebounce($event.target.value)" class="custom-search-input" type="search" placeholder="Seach...">
-        </div>
-        <div v-if="!fileDownloading">
-          <b-button v-if="fileContents !== ''" size="sm" @click="downloadFile(fileContents, fileDownloadingName)"><b-icon icon="download"></b-icon> Download</b-button>
-          <b-button v-else size="sm" disabled><b-icon icon="download"></b-icon> Download</b-button>
-
-          <b-button size="sm" v-b-modal.editor-fullscreen style="margin-left: 5px;"><b-icon icon="arrows-fullscreen"></b-icon> Fullscreen</b-button>
-        </div>
+    <div class="d-flex justify-content-between" style="margin-bottom:5px;">
+      <div class="search-area">
+          <b-icon class="float-left search-icon" icon="search"></b-icon>
+          <input v-if="treeLoaded" @input="inputDebounce($event.target.value)" class="custom-search-input" type="search" placeholder="Seach...">
+          <input v-else disabled class="custom-search-input" type="search" placeholder="Seach...">
       </div>
-      <div class="row">
-        <div class="col-4" style="overflow-y:scroll;overflow-x:hidden;max-height:60vh;">
-          <div v-if="displayTree && tree.children != null && tree.children.length > 0">
-            <vue-tree-list :model="tree" @click="nodeClicked" v-bind:default-expanded="false" default-tree-node-name="new folder" default-leaf-node-name="new file">
-              <template v-slot:treeNodeIcon="node">
-                <span v-if="!node.expanded" class="node-icon">
-                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"></path></g></svg>
-                </span>
-                <span v-else class="node-icon">
-                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder2 open" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder2-open mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"></path></g></svg>
-                </span>
-              </template>
-              <template v-slot:leafNodeIcon="node">
-                <span v-if="node.model.size <= 100000 && downloadOnly.indexOf(node.model.name.split('.').pop()) === -1" class="node-icon">
-                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="file earmark code fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-earmark-code-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.646 7.646a.5.5 0 1 1 .708.708L5.707 10l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 10 8.646 8.354a.5.5 0 1 1 .708-.708z"></path></g></svg>
-                </span>
-                <span v-else class="node-icon">
-                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="file arrow down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-arrow-down-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z"></path></g></svg>
-                </span>
-              </template>
-            </vue-tree-list>
-          </div>
+      <div v-if="!fileDownloading">
+        <b-button v-if="fileContents !== ''" size="sm" @click="downloadFile(fileContents, fileDownloadingName)"><b-icon icon="download"></b-icon> Download</b-button>
+        <b-button v-else size="sm" disabled><b-icon icon="download"></b-icon> Download</b-button>
+
+        <b-button size="sm" v-b-modal.editor-fullscreen style="margin-left: 5px;"><b-icon icon="arrows-fullscreen"></b-icon> Fullscreen</b-button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4" style="overflow-y:scroll;overflow-x:hidden;max-height:60vh;">
+        <div v-if="treeLoaded">
+          <vue-tree-list v-if="tree.children != null && tree.children.length > 0" :model="tree" @click="nodeClicked" v-bind:default-expanded="false" default-tree-node-name="new folder" default-leaf-node-name="new file">
+            <template v-slot:treeNodeIcon="node">
+              <span v-if="!node.expanded" class="node-icon">
+                <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"></path></g></svg>
+              </span>
+              <span v-else class="node-icon">
+                <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder2 open" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder2-open mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"></path></g></svg>
+              </span>
+            </template>
+            <template v-slot:leafNodeIcon="node">
+              <span v-if="node.model.size <= 100000 && downloadOnly.indexOf(node.model.name.split('.').pop()) === -1" class="node-icon">
+                <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="file earmark code fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-earmark-code-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.646 7.646a.5.5 0 1 1 .708.708L5.707 10l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 10 8.646 8.354a.5.5 0 1 1 .708-.708z"></path></g></svg>
+              </span>
+              <span v-else class="node-icon">
+                <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="file arrow down fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-arrow-down-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8 5a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5A.5.5 0 0 1 8 5z"></path></g></svg>
+              </span>
+            </template>
+          </vue-tree-list>
           <h5 v-else class="text-center" style="margin-top: 15px;">No result...</h5>
         </div>
-        <div class="col-8">
-          <div v-if="fileDownloading" class="d-flex justify-content-center mb-3 CodeMirror" style="background-color:var(--dathost-dark-dark);">
-            <b-spinner v-if="fileDownloading" style="width: 6rem; height: 6rem; margin-top: 25px;color:#adafae;" label="Loading..."></b-spinner>
-          </div>
-          <div v-else>
-            <b-modal :hide-footer="true" id="editor-fullscreen">
-              <codemirror :options="editorOptions" v-model="fileContents" />
-              <div class="d-flex justify-content-end mb-3">
-              <template v-if="!fileUploading">
-                <b-button v-if="!ogContents" @click="saveFileChanges()" variant="primary"><b-icon icon="stickies"></b-icon> Save</b-button>
-                <b-button v-else disabled variant="primary"><b-icon icon="stickies"></b-icon> Save</b-button>
-              </template>
-              <b-button v-else disabled variant="primary"><b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner> Saving</b-button>
-              </div>
-            </b-modal>
+        <div v-else class="d-flex justify-content-center mb-3">
+          <b-spinner style="width: 6rem; height: 6rem; margin-top: 25px;" label="Loading..."></b-spinner>
+        </div>
+      </div>
+      <div class="col-8">
+        <div v-if="fileDownloading" class="d-flex justify-content-center mb-3 CodeMirror" style="background-color:var(--dathost-dark-dark);">
+          <b-spinner v-if="fileDownloading" style="width: 6rem; height: 6rem; margin-top: 25px;color:#adafae;" label="Loading..."></b-spinner>
+        </div>
+        <div v-else>
+          <b-modal :hide-footer="true" id="editor-fullscreen">
             <codemirror :options="editorOptions" v-model="fileContents" />
-
-            <div class="d-flex justify-content-between" style="margin-top:5px;">
-              <b-dropdown size="sm">
-                <template #button-content>
-                  Theme: <strong class="text-capitalize">{{ editorOptions.theme.replaceAll('-', ' ') }}</strong>
-                </template>
-                <template v-for="theme in themes">
-                  <b-dropdown-item v-if="theme !== editorOptions.theme" @click="setTheme(theme)" :key="theme" class="text-capitalize">{{ theme.replaceAll('-', ' ') }}</b-dropdown-item>
-                </template>
-              </b-dropdown>
-              <template v-if="!fileUploading">
-                <b-button v-if="!ogContents" @click="saveFileChanges()" style="width: 20%;" variant="primary" size="sm"><b-icon icon="stickies"></b-icon> Save</b-button>
-                <b-button v-else disabled style="width: 20%;" variant="primary" size="sm"><b-icon icon="stickies"></b-icon> Save</b-button>
-              </template>
-              <b-button v-else disabled style="width: 20%;" variant="primary" size="sm"><b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner> Saving</b-button>
+            <div class="d-flex justify-content-end mb-3">
+            <template v-if="!fileUploading">
+              <b-button v-if="!ogContents" @click="saveFileChanges()" variant="primary"><b-icon icon="stickies"></b-icon> Save</b-button>
+              <b-button v-else disabled variant="primary"><b-icon icon="stickies"></b-icon> Save</b-button>
+            </template>
+            <b-button v-else disabled variant="primary"><b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner> Saving</b-button>
             </div>
+          </b-modal>
+          <codemirror :options="editorOptions" v-model="fileContents" />
+
+          <div class="d-flex justify-content-between" style="margin-top:5px;">
+            <b-dropdown size="sm">
+              <template #button-content>
+                Theme: <strong class="text-capitalize">{{ editorOptions.theme.replaceAll('-', ' ') }}</strong>
+              </template>
+              <template v-for="theme in themes">
+                <b-dropdown-item v-if="theme !== editorOptions.theme" @click="setTheme(theme)" :key="theme" class="text-capitalize">{{ theme.replaceAll('-', ' ') }}</b-dropdown-item>
+              </template>
+            </b-dropdown>
+            <template v-if="!fileUploading">
+              <b-button v-if="!ogContents" @click="saveFileChanges()" style="width: 20%;" variant="primary" size="sm"><b-icon icon="stickies"></b-icon> Save</b-button>
+              <b-button v-else disabled style="width: 20%;" variant="primary" size="sm"><b-icon icon="stickies"></b-icon> Save</b-button>
+            </template>
+            <b-button v-else disabled style="width: 20%;" variant="primary" size="sm"><b-spinner label="Spinning" style="width: 1.4em; height: 1.4em;"></b-spinner> Saving</b-button>
           </div>
         </div>
       </div>
     </div>
-    <div v-else class="d-flex justify-content-center mb-3">
-      <b-spinner style="width: 6rem; height: 6rem; margin-top: 25px;" label="Loading..."></b-spinner>
-    </div>
-
     <div>
       <h5 style="margin-top:10px;margin-bottom:5px;">FTP Details</h5>
       <p @click="copyToClipboard(server.ip)" v-b-tooltip.hover.left title="Copy to clipboard" style="margin:0;cursor: pointer;">
@@ -146,9 +144,6 @@ export default class ServerFileComp extends VueMixin {
 
   inputDebounce: ReturnType<typeof debounce>
 
-  // Needed to force reload tree when searching.
-  displayTree = true
-
   editorOptions = {
     lineNumbers: true,
     line: true,
@@ -199,6 +194,15 @@ export default class ServerFileComp extends VueMixin {
   async mounted (): Promise<void> {
     this.ftpPassword = this.server.ftp_password
 
+    this.inputDebounce = debounce(this.searchNodes, 500)
+
+    const editorTheme = localStorage.getItem('editorTheme')
+    if (editorTheme) {
+      this.setTheme(editorTheme)
+    } else {
+      this.setTheme('lesser-dark')
+    }
+
     this.treeLoaded = false
     this.tree = new Tree([])
 
@@ -214,15 +218,6 @@ export default class ServerFileComp extends VueMixin {
 
     // Clone tree object.
     this.treeBackup = clone(this.tree)
-
-    this.inputDebounce = debounce(this.searchNodes, 500)
-
-    const editorTheme = localStorage.getItem('editorTheme')
-    if (editorTheme) {
-      this.setTheme(editorTheme)
-    } else {
-      this.setTheme('lesser-dark')
-    }
 
     this.treeLoaded = true
   }
@@ -374,7 +369,7 @@ export default class ServerFileComp extends VueMixin {
   }
 
   async searchNodes (search: string): Promise<void> {
-    this.displayTree = false
+    this.treeLoaded = false
 
     if (search !== '') {
       const searchLower = search.toLowerCase()
@@ -399,7 +394,7 @@ export default class ServerFileComp extends VueMixin {
       this.tree = clone(this.treeBackup)
     }
 
-    this.displayTree = true
+    this.treeLoaded = true
   }
 
   subTreeAdd (file: IFile, tree = this.tree, dir?: string): void {
