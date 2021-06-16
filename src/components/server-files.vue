1fac130f5a55a4795e615480aad8f74498ea2da1
Ward <wardpearce@protonmail.com>
2021-06-16 15:45:02 +1200

    Added custom node icons without causing lag

--- src/components/server-files.vue
+++ src/components/server-files.vue
@@ -15,6 +15,19 @@
         <div class="col-4" style="overflow-y:scroll;overflow-x:hidden;max-height:60vh;">
           <div v-if="displayTree && tree.children != null && tree.children.length > 0">
             <vue-tree-list :model="tree" @click="nodeClicked" v-bind:default-expanded="false" default-tree-node-name="new folder" default-leaf-node-name="new file">
+              <template v-slot:treeNodeIcon="node">
+                <span v-if="!node.expanded" class="node-icon">
+                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"></path></g></svg>
+                </span>
+                <span v-else class="node-icon">
+                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="folder2 open" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-folder2-open mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"></path></g></svg>
+                </span>
+              </template>
+              <template v-slot:leafNodeIcon>
+                <span class="node-icon">
+                  <svg viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="file earmark code fill" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-file-earmark-code-fill mx-auto b-icon bi" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;"><g data-v-41be6633=""><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.646 7.646a.5.5 0 1 1 .708.708L5.707 10l1.647 1.646a.5.5 0 0 1-.708.708l-2-2a.5.5 0 0 1 0-.708l2-2zm2.708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 10 8.646 8.354a.5.5 0 1 1 .708-.708z"></path></g></svg>
+                </span>
+              </template>
             </vue-tree-list>
           </div>
           <h5 v-else class="text-center" style="margin-top: 15px;">No result...</h5>
